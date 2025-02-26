8240362b1787be8db19d0c1e1b6167d64a3e355f
Joao Henrique Gadea <joaohgadea@gmail.com>
2025-02-26 19:20:42 -0300

    Add files via upload

---
+++ Aula 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	printf("========== BOAS VINDAS ============\n======= Primeira aula de C ========\n======== Um abraço a todos ========");
+	getchar();
+	getchar();
+}
